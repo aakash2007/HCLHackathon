6f8b29624ad38f90ef6edefdd5294611090671fc
Aakash Bajaj <f2015586@pilani.bits-pilani.ac.in>
2017-07-12 02:02:12 +0530

    Added Greeting

---
+++ app/recognizers/gretting.js
@@ -0,0 +1,17 @@
+const greetings = [
+    'hi',
+    'hi there',
+    'hey',
+    'hey there',
+    'hello',
+    'hello hello',
+    'hello there',
+    'what up',
+    'what\'s up',
+    'whatup',
+    'salute',
+    'morning',
+    'good morning',
+    'how are you',
+    'how r u'
+];
