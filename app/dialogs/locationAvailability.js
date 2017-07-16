8ccafc6cac2e9943e60df4d7e869bb2f696ce6a2
Aakash Bajaj <aakashbajaj5311@gmail.com>
2017-07-16 20:33:56 +0530

    Another Minor Typo

--- app/dialogs/locationAvailability.js
+++ app/dialogs/locationAvailability.js
@@ -30,7 +30,7 @@ module.exports = function(bot) {
 				session.send('Yay! we are available in your city')
     			session.endDialog()
 	    	} else {
-	    		session.send('Sorry! we wil be available in your city soon')
+	    		session.send('Sorry! we will be available in your city soon')
     			session.endDialog()
 	    	}
     	}
@@ -39,7 +39,7 @@ module.exports = function(bot) {
 	    "restartlocationAvailability", "Ok. Let's start over.",
 	    {
 	        matches: /^start over$/i,
-	        confirmPrompt: "This wil cancel start over. Are you sure?"
+	        confirmPrompt: "This will cancel start over. Are you sure?"
 	    }
 	)
 	.cancelAction(
